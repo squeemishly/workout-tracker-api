68ff502050ac11e12561ffdf98b636ab2c5a1a3d
squeemishly <squeemishly@gmail.com>
2017-09-11 13:27:15 -0600

    creates get /api/v1/users/1/workouts test

---
+++ db/seeds/test/07-workouts.js
@@ -0,0 +1,31 @@
+exports.seed = (knex, Promise) => {
+  return knex.raw('TRUNCATE workouts RESTART IDENTITY CASCADE')
+  .then( () => {
+    return Promise.all([
+      knex.raw(
+        'INSERT INTO workouts (user_id, focus_area, workout_date, created_at) VALUES (?, ?, ?, ?)',
+        [2, "Hamstrings", 'Sept-22-2017', new Date]
+      ),
+      knex.raw(
+        'INSERT INTO workouts (user_id, focus_area, workout_date, created_at) VALUES (?, ?, ?, ?)',
+        [2, "Chest", 'Sept-23-2017', new Date]
+      ),
+      knex.raw(
+        'INSERT INTO workouts (user_id, focus_area, workout_date, created_at) VALUES (?, ?, ?, ?)',
+        [3, "Bench", 'Sept-20-2017', new Date]
+      ),
+      knex.raw(
+        'INSERT INTO workouts (user_id, focus_area, workout_date, created_at) VALUES (?, ?, ?, ?)',
+        [3, "Squat", 'Sept-23-2017', new Date]
+      ),
+      knex.raw(
+        'INSERT INTO workouts (user_id, focus_area, workout_date, created_at) VALUES (?, ?, ?, ?)',
+        [4, "Squat", 'Sept-22-2017', new Date]
+      ),
+      knex.raw(
+        'INSERT INTO workouts (user_id, focus_area, workout_date, created_at) VALUES (?, ?, ?, ?)',
+        [4, "Shoulders", 'Sept-27-2017', new Date]
+      ),
+    ])
+  })
+}
---
+++ test/workouts-test.js
@@ -0,0 +1,65 @@
+const assert = require('chai').assert
+const request = require('request')
+const app = require('../server')
+
+const environment = process.env.NODE_ENV || 'test'
+const configuration = require('../knexfile')[environment]
+const database = require('knex')(configuration)
+
+
+describe('Server', () => {
+  before( done => {
+    this.port = 9876
+    this.server = app.listen(this.port, (err, result) => {
+      if (err) { return done(err); }
+      done()
+    })
+
+    this.request = request.defaults({
+      baseUrl: 'http://localhost:9876'
+    })
+  })
+
+  beforeEach(done => {
+    database.seed.run()
+    .then(() => done())
+  })
+
+  afterEach(done => {
+    Promise.all([
+      database.raw(`TRUNCATE lifts RESTART IDENTITY CASCADE`),
+      database.raw(`TRUNCATE bodyareas RESTART IDENTITY CASCADE`),
+      database.raw(`TRUNCATE bodyarea_lifts RESTART IDENTITY CASCADE`),
+      database.raw(`TRUNCATE workouts RESTART IDENTITY CASCADE`),
+      database.raw(`TRUNCATE workout_lifts RESTART IDENTITY CASCADE`),
+      database.raw(`TRUNCATE sets RESTART IDENTITY CASCADE`),
+      database.raw(`TRUNCATE roles RESTART IDENTITY CASCADE`),
+      database.raw(`TRUNCATE users RESTART IDENTITY CASCADE`),
+    ])
+    .then(() => done())
+  })
+
+  after( () => {
+    this.server.close()
+  })
+
+  describe('GET /api/v1/users/:id/workouts', () => {
+    it('should return a 200', done => {
+      this.request.get('/api/v1/users/1/workouts', (err, res) => {
+        if(err) { return done(err) }
+        assert.equal(res.statusCode, 200)
+        done()
+      })
+    })
+
+    it('should return a list of workouts for a particular user', done => {
+      this.request.get('/api/v1/users/1/workouts', (err, res) => {
+        if(err) { return done(err) }
+        const workouts = JSON.parse(res.body)
+        assert.equal(workouts.length, 2)
+        assert.hasAllKeys(workouts[0], ["Date", "Focus", "Lifts"])
+        done()
+      })
+    })
+  })
+})
