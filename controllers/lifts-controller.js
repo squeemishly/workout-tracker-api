cfa972eeaf587b82136b2be14319f2b0ace47a65
squeemishly <squeemishly@gmail.com>
2017-09-10 19:26:52 -0600

    moves all lift logic out of the server and into the LiftsController

--- controllers/lifts-controller.js
+++ controllers/lifts-controller.js
@@ -10,6 +10,62 @@ class LiftsController {
     })
   }
 
+  static findLift(req, res) {
+    const { id } = req.params
+    database.raw(`SELECT id, name FROM lifts WHERE id = ?`, [id])
+    .then( data => {
+      if (data.rows.length < 1) {
+        res.sendStatus(404)
+      } else {
+        return res.json(data.rows)
+      }
+    })
+  }
+
+  static createLift(req, res) {
+    const lift = req.body.lift
+    const name = lift.name
+
+    if (name === "") {
+      res.sendStatus(400)
+    } else {
+      database.raw(`INSERT INTO lifts (name, created_at) VALUES (?, ?) RETURNING id, name`, [name, new Date])
+      .then( data => {
+        return res.json(data.rows)
+      })
+    }
+  }
+
+  static updateLift(req, res) {
+    const { id } = req.params
+    const lift = req.body.lift
+    const name = lift.name
+
+    if (name === "") {
+      res.sendStatus(400)
+    } else {
+      database.raw(`UPDATE lifts SET name = ? WHERE id = ? RETURNING id, name`, [name, id])
+      .then( data => {
+        if (data.rows.length < 1) {
+            res.sendStatus(404)
+        } else {
+          return res.json(data.rows)
+        }
+      })
+    }
+  }
+
+  static deleteLift(req, res) {
+    const { id } = req.params
+    database.raw(`DELETE FROM lifts WHERE id = ?`, [id])
+    .then(data => {
+      if (data.rowCount < 1) {
+        res.sendStatus(404)
+      } else {
+        res.sendStatus(200)
+      }
+    })
+  }
 }
 
 module.exports = LiftsController
