7ac04c01d5a2187c54022063094bc1b0ac163188
squeemishly <squeemishly@gmail.com>
2017-09-12 19:52:22 -0600

    moves database calls and logic to a Workouts model for getUserWorkouts

--- controllers/workouts-controller.js
+++ controllers/workouts-controller.js
@@ -1,3 +1,5 @@
+const Workouts = require('../models/workouts')
+
 const environment = process.env.NODE_ENV || 'development'
 const configuration = require('../knexfile')[environment]
 const database = require('knex')(configuration)
@@ -5,12 +7,12 @@ const database = require('knex')(configuration)
 class WorkoutsController {
   static getUserWorkouts(req, res) {
     const id = req.params.user_id
-    database.raw(`SELECT id, focus_area AS focus, workout_date AS date FROM workouts WHERE user_id = ?`, [id])
+    Workouts.getWorkoutInfo(id)
     .then( data => {
       const workouts = []
       data.rows.forEach(workout => {
         const workoutId = workout.id
-        database.raw(`SELECT lifts.name, sets.reps, sets.weight FROM workout_lifts JOIN lifts ON workout_lifts.lift_id = lifts.id JOIN sets ON workout_lifts.id = sets.workout_lifts_id WHERE workout_lifts.id = ? ORDER BY workout_lifts.id`, [workoutId])
+        Workouts.getLiftInfo(workoutId)
         .then( singleWorkoutData => {
           const lifts = []
           singleWorkoutData.rows.forEach( aLift => {
