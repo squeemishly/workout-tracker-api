5ea86be583f505ef70b281aa7601a5565316e6f3
squeemishly <squeemishly@gmail.com>
2017-09-16 20:25:32 -0600

    creates check to make sure user is verified to see information they should

--- controllers/workouts-controller.js
+++ controllers/workouts-controller.js
@@ -1,4 +1,5 @@
 const Workouts = require('../models/workouts')
+const User = require('../models/users')
 
 const environment = process.env.NODE_ENV || 'development'
 const configuration = require('../knexfile')[environment]
@@ -6,25 +7,33 @@ const database = require('knex')(configuration)
 
 class WorkoutsController {
   static getUserWorkouts(req, res) {
+    const token = req.query.token
     const id = req.params.user_id
-    Workouts.getWorkoutInfo(id)
-    .then( workouts =>
-      Promise.all([
-        workouts.rows,
-        Promise.all(workouts.rows.map(workout => Workouts.getLiftInfo(workout.id) ))
-      ])
-    )
-    .then(([workouts, allLifts]) => {
-      const workoutObjects = workouts.map( workout => {
-        const liftsData = allLifts.find(lifts => lifts.rows.some(lift => lift.workout_id === workout.id))
-        const lifts = liftsData.rows
-        return { "id": workout.id, "date": workout.date, "focus": workout.focus, "lifts": lifts }
+    const verifiedUser = User.verifyUser(id, token)
+    if (verifiedUser) {
+      console.log(verifiedUser)
+      Workouts.getWorkoutInfo(id)
+      .then( workouts =>
+        Promise.all([
+          workouts.rows,
+          Promise.all(workouts.rows.map(workout => Workouts.getLiftInfo(workout.id) ))
+        ])
+      )
+      .then(([workouts, allLifts]) => {
+        const workoutObjects = workouts.map( workout => {
+          const liftsData = allLifts.find(lifts => lifts.rows.some(lift => lift.workout_id === workout.id))
+          const lifts = liftsData.rows
+          return { "id": workout.id, "date": workout.date, "focus": workout.focus, "lifts": lifts }
+        })
+        return workoutObjects
       })
-      return workoutObjects
-    })
-    .then( workouts => {
-      res.json(workouts)
-    })
+      .then( workouts => {
+        res.json(workouts)
+      })
+    } else {
+      console.log(verifiedUser)
+      res.sendStatus(404)
+    }
   }
 }
 
